5461aafec9d8b1713df3c3cabf6e9ce5e0d36969
Wesley Sousa <wesleyltsousa@gmail.com>
2025-08-19 08:55:31 -0300

    Project created

---
+++ src/test/java/Wesley2v/com/github/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package Wesley2v.com.github.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
